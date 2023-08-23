280662a89e74dd900920d9789f1797d6d0f37083
Tejal Bhoi <tejalbhoi27@gmail.com>
2023-08-23 20:11:50 +0530

    by tejal

---
+++ Tejal Bhoi/src/Info.java
@@ -0,0 +1,17 @@
+
+public class Info 
+{
+public String name()
+{
+	return("Tejal");
+}
+public String add()
+{
+	return("BHUSAWAL");
+}
+public String mobile()
+{
+	return("9284347574");
+}
+}
+
---
+++ Tejal Bhoi/src/Result.java
@@ -0,0 +1,15 @@
+
+
+public class Result
+{
+public static void main (String[] args)
+{
+Info i=new Info();
+String x=i.name();
+String y=i.add();
+String z=i.mobile();
+System.out.println("Name="+x);
+System.out.println("Add="+y);
+System.out.println("mobile="+z);
+}
+}
